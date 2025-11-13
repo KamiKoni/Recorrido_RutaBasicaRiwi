65ce6c21a34342335b8a827b5f1bea1496eff777
argenis <argenisdavid212@gmail.com>
2025-11-13 11:27:30 -0500

    SevenCommit

--- Proyecto_RutaBasica/Modulo1_Semana 2/ejercicios_listas_dic.py
+++ Proyecto_RutaBasica/Modulo1_Semana 2/ejercicios_listas_dic.py
@@ -65,12 +65,12 @@ for clave, valor in curso_python.items():
 nivel = curso_python.get("nivel", "principiante")
 print("Nivel del curso:", nivel)
 
-for i in range (1, 100):
+for i in range (1, 101):
+    if(i % 3 == 0 and i % 5 == 0):
+        print("fizzbuzz")    
     if(i % 3 == 0):
         print("fizz")
     if(i % 5 == 0):
         print("buzz")    
-    if(i % 3 == 0 and i & 5 == 0):
-        print("fizzbuzz")    
     else:
-        print(i)s
+        print(i)
