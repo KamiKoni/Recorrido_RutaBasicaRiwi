432145fa26025ad82d5cb36459915914aa0195d8
argenis <argenisdavid212@gmail.com>
2025-11-22 18:22:58 -0500

    fourteen_commit

---
+++ Proyecto_RutaBasica/Modulo1_Semana 3/historia_usuario/Loading.py
@@ -0,0 +1,27 @@
+import os
+import platform
+import time
+def load_bar():
+    while True:
+        print("⬛⬜⬜⬜⬜⬜⬜")
+        time.sleep(0.05)
+        os.system('cls')
+        print("⬛⬛⬜⬜⬜⬜⬜")
+        time.sleep(0.05)
+        os.system('cls')
+        print("⬛⬛⬛⬜⬜⬜⬜")
+        time.sleep(0.05)
+        os.system('cls')
+        print("⬛⬛⬛⬛⬜⬜⬜")
+        time.sleep(0.05)
+        os.system('cls')
+        print("⬛⬛⬛⬛⬛⬜⬜")
+        time.sleep(0.05)
+        os.system('cls')
+        print("⬛⬛⬛⬛⬛⬛⬜")
+        time.sleep(0.05)
+        os.system('cls')
+        print("⬛⬛⬛⬛⬛⬛⬛")
+        time.sleep(0.05)
+        os.system('cls')
+        break
